e4bdd348a74d832530c99cba7029cf6a401d6658
Jeff Taggart <jeff@jetaggart.com>
2014-07-10 12:07:45 -0600

    heroku-ify the application

--- lib/database_connection.rb
+++ lib/database_connection.rb
@@ -2,15 +2,24 @@ require "yaml"
 require "active_record"
 
 class DatabaseConnection
-  attr_reader :config, :connection
+  attr_reader :connection
 
-  def self.establish(environment)
-    @@_connection ||= new(environment)
+  def self.establish(environment = "development", config_file_path="config/database.yml")
+    @@_connection ||= new(environment, config_file_path)
   end
 
-  def initialize(environment = "development")
-    @config = YAML.load(File.read("config/database.yml"))[environment]
-    @connection = establish_connection
+  def self.clear!
+    @@_connection = nil
+  end
+
+  def initialize(environment, config_file_path)
+    begin
+      file = File.read(config_file_path)
+      config = YAML.load(file)[environment]
+      @connection = establish_from_config(config)
+    rescue Errno::ENOENT
+      @connection = establish_from_uri(ENV["DATABASE_URL"])
+    end
   end
 
   def sql(sql_string)
@@ -20,12 +29,16 @@ class DatabaseConnection
   private_class_method :new
   private
 
-  def establish_connection
+  def establish_from_config(config)
     ActiveRecord::Base.establish_connection(
-      :adapter  => config['adapter'],
+      :adapter => config['adapter'],
       :database => config['database'],
       :username => config['username'],
       :password => config['password']
     ).connection
+  end
+
+  def establish_from_uri(uri)
+    ActiveRecord::Base.establish_connection(uri).connection
   end
 end
