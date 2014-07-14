3afec720d4d2f58b536653b4ffb01a675b232008
Shelby Kelly <shelby.kelly@colorado.edu>
2014-07-14 11:22:04 -0600

    Updating

--- app.rb
+++ app.rb
@@ -24,6 +24,7 @@ class App < Sinatra::Application
   get "/login" do
     @user = @database_connection.sql("select username from users where id = #{session[:user_id]}").first
     erb :login, :locals => {:user => @user}
+
   end
 
   get "/logout" do
@@ -31,16 +32,46 @@ class App < Sinatra::Application
   end
 
 
-
+  # if (params[:username] || params[:password]) == ""
+  # flash[:error] = "Please fill in all fields."
+  # redirect "/"
+  # end
 
 
   post "/login" do
+
+    if params[:username] == "" && params[:password] == ""
+    flash[:error] = "Username and password required."
+    redirect "/"
+
+    elsif params[:username] == ""
+    flash[:error] = "Username required."
+    redirect "/"
+
+    elsif params[:password] == ""
+    flash[:error] = "Password required."
+    redirect "/"
+
+    end
+
     user = @database_connection.sql("select * from users where username = '#{params[:username]}' and password = '#{params[:password]}'").first
     session[:user_id] = user["id"]
     redirect "/login"
+
+
   end
 
   post "/register" do
+
+    if (params[:username] || params[:password]) == ""
+      flash[:error] = "Please fill in all fields."
+      redirect "/register"
+    elsif  @database_connection.sql("SELECT * FROM users WHERE username = '#{params[:username].downcase}'") != []
+      flash[:error] = "Username is already taken."
+      redirect "/register"
+    end
+
+
     flash[:notice] = "Thank you for registering"
     @database_connection.sql("INSERT INTO users (username, password) VALUES ('#{params[:username]}','#{params[:password]}')")
     redirect "/"
@@ -52,6 +83,7 @@ class App < Sinatra::Application
   def find_user(params)
     @database_connection.all.select { |x| x[:username] == params[:username] && x[:password] == params[:password] }[0]
   end
+
 
 
 end
