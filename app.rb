7f09a8875eaf59f1c733d23ca6f10c7ef2d8aa48
Rick Walker <walkrick@yahoo.com>
2014-07-14 16:58:40 -0600

    more stories down

--- app.rb
+++ app.rb
@@ -14,20 +14,27 @@ class App < Sinatra::Application
   end
 
   get "/" do
-    erb :root
+    if session[:user_id]
+      current_user = @database_connection.sql("select username from users where id = #{session[:user_id]}").first
+      other_users = @database_connection.sql("select username from users where id != #{session[:user_id]}")
+      erb :logged_in, :locals => {:user => @user, :current_user => current_user, :other_users => other_users}
+    else
+      erb :root
+    end
   end
 
   get "/register" do
     erb :register
   end
 
-  get "/login" do
+  get "/loggged_in" do
     @user = @database_connection.sql("select username from users where id = #{session[:user_id]}").first
     erb :login, :locals => {:user => @user}
 
   end
 
   get "/logout" do
+    session.delete(:user_id)
     redirect "/"
   end
 
@@ -41,40 +48,42 @@ class App < Sinatra::Application
   post "/login" do
 
     if params[:username] == "" && params[:password] == ""
-    flash[:error] = "Username and password required."
-    redirect "/"
+      flash[:error] = "Username and password required."
+      redirect "/"
 
     elsif params[:username] == ""
-    flash[:error] = "Username required."
-    redirect "/"
+      flash[:error] = "Username required."
+      redirect "/"
 
     elsif params[:password] == ""
-    flash[:error] = "Password required."
-    redirect "/"
+      flash[:error] = "Password required."
+      redirect "/"
 
+    else
+      user = @database_connection.sql("select id from users where username = '#{params[:username]}' and password = '#{params[:password]}'").first
+      session[:user_id] = user["id"]
+      redirect "/"
     end
-
-    user = @database_connection.sql("select * from users where username = '#{params[:username]}' and password = '#{params[:password]}'").first
-    session[:user_id] = user["id"]
-    redirect "/login"
 
 
   end
 
   post "/register" do
 
-    if (params[:username] || params[:password]) == ""
-      flash[:error] = "Please fill in all fields."
+    if params[:username] == ""
+      flash[:error] = "Please fill in username."
       redirect "/register"
-    elsif  @database_connection.sql("SELECT * FROM users WHERE username = '#{params[:username].downcase}'") != []
+    elsif params[:password] == ""
+      flash[:error] = "Please fill in password."
+      redirect "/register"
+    elsif @database_connection.sql("SELECT * FROM users WHERE username = '#{params[:username].downcase}'") != []
       flash[:error] = "Username is already taken."
       redirect "/register"
+    else
+      @database_connection.sql("INSERT INTO users (username, password) VALUES ('#{params[:username]}','#{params[:password]}')")
+      flash[:notice] = "Thank you for registering"
+      redirect "/"
     end
-
-
-    flash[:notice] = "Thank you for registering"
-    @database_connection.sql("INSERT INTO users (username, password) VALUES ('#{params[:username]}','#{params[:password]}')")
-    redirect "/"
   end
 
 
@@ -83,7 +92,6 @@ class App < Sinatra::Application
   def find_user(params)
     @database_connection.all.select { |x| x[:username] == params[:username] && x[:password] == params[:password] }[0]
   end
-
 
 
 end
